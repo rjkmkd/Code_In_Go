142e0a169114aeabe141145589e767704a63afcb
Rajkumar <rajkumarkundu212@gmail.com>
2024-10-18 18:34:48 +0530

    add basics of pointer

---
+++ 13_variadic_function/variadic.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// total := sum(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+	total:=[]int{10,20,30}
+	fmt.Println(sum(total...))
+}
+
+func sum(values ...int) int {
+	total := 0
+	for _,val := range values {
+		total += val
+		// fmt.Println(val)
+	}
+	return total
+}
