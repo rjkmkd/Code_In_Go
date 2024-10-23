ea9158f85e6fe7483f1889d9c7a1700470c21ab5
Rajkumar <rajkumarkundu212@gmail.com>
2024-10-23 21:27:37 +0530

    Add basic syntax

---
+++ 9_slices/slice.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	// "slices"
+)
+
+func main() {
+	//uninitilized slice is nil
+	// var nums []int
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+
+	// var nums = make([]int,0,3) //one more syntax
+
+	// nums:=[]int{1,2,3,4}
+	// // nums = append(nums, 2)
+	// // nums = append(nums, 4)
+	// // nums = append(nums, 6)
+	// // nums = append(nums, 6)
+	// nums = append(nums, 6)
+	// nums[4]=3
+	// fmt.Println(nums)
+	// fmt.Println(len(nums))
+	// fmt.Println(cap(nums))
+
+	// slice operator
+
+	// var nums[]int
+	// nums=[]int{1,2,3,4}
+	// fmt.Println(nums[0:]) // output => [1 2 3 4]
+	// fmt.Println(nums[0:]) // output => [1 2 3 4]
+	// fmt.Println(nums[0:2]) // output => [1 2 ]
+
+	//slices pagkage
+
+	// var num1 = []int{1,2,3}
+	// var num2 = []int{1,2,3}
+
+	// fmt.Println(slices.Equal(num1,num2)) // true
+
+	// var num1 = []int{1,2,3}
+	//  var num2 = make([]int,len(num1))
+	// copy(num2, num1)
+	// fmt.Println(num1)
+	// fmt.Println(num2)
+
+
+ }
