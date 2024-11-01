22fed1d4cd01941f6b8a7f1dc76af40b08c17159
Rajkumar <rajkumarkundu212@gmail.com>
2024-11-01 23:56:17 +0530

    update notes

--- 9_slices/slice.go
+++ 9_slices/slice.go
@@ -1,49 +1,43 @@
 package main
 
 import (
-	"fmt"
-	// "slices"
+	// "fmt"
+	// "slices" // Importing "slices" package for slice utility functions like Equal
 )
 
 func main() {
-	//uninitilized slice is nil
+	// A slice with no initial values is `nil` and has length 0.
 	// var nums []int
-	// fmt.Println(nums)
-	// fmt.Println(len(nums))
+	// fmt.Println(nums)       // Output: [] (nil slice)
+	// fmt.Println(len(nums))   // Output: 0 (length of the slice)
 
-	// var nums = make([]int,0,3) //one more syntax
+	// Another way to initialize a slice with `make` (initial length: 0, capacity: 3).
+	// var nums = make([]int, 0, 3)
 
-	// nums:=[]int{1,2,3,4}
-	// // nums = append(nums, 2)
-	// // nums = append(nums, 4)
-	// // nums = append(nums, 6)
-	// // nums = append(nums, 6)
-	// nums = append(nums, 6)
-	// nums[4]=3
-	// fmt.Println(nums)
-	// fmt.Println(len(nums))
-	// fmt.Println(cap(nums))
+	// Initializing and using a slice with values.
+	// nums := []int{1, 2, 3, 4}      // `nums` is a slice with four elements.
+	// nums = append(nums, 6)         // Appending an element increases the slice's length.
+	// nums[4] = 3                    // Directly modifying an element in the slice
+	// fmt.Println(nums)              // Output: [1 2 3 4 3]
+	// fmt.Println(len(nums))         // Output: 5 (current length of the slice)
+	// fmt.Println(cap(nums))         // Output: capacity of the slice, dynamically managed by Go
 
-	// slice operator
+	// Using the slice operator `[:]` to extract portions of a slice.
+	// var nums []int
+	// nums = []int{1, 2, 3, 4}
+	// fmt.Println(nums[0:])          // Output: [1 2 3 4] (entire slice from index 0 onward)
+	// fmt.Println(nums[0:2])         // Output: [1 2] (slice from index 0 up to, but not including, index 2)
 
-	// var nums[]int
-	// nums=[]int{1,2,3,4}
-	// fmt.Println(nums[0:]) // output => [1 2 3 4]
-	// fmt.Println(nums[0:]) // output => [1 2 3 4]
-	// fmt.Println(nums[0:2]) // output => [1 2 ]
+	// Using the `slices` package (commented out because "slices" is not standard).
+	// The `slices` package provides functions like `Equal` to compare slices.
+	// var num1 = []int{1, 2, 3}
+	// var num2 = []int{1, 2, 3}
+	// fmt.Println(slices.Equal(num1, num2)) // Output: true (both slices are equal)
 
-	//slices pagkage
-
-	// var num1 = []int{1,2,3}
-	// var num2 = []int{1,2,3}
-
-	// fmt.Println(slices.Equal(num1,num2)) // true
-
-	// var num1 = []int{1,2,3}
-	//  var num2 = make([]int,len(num1))
-	// copy(num2, num1)
-	// fmt.Println(num1)
-	// fmt.Println(num2)
-
-
- }
+	// Copying elements from one slice to another.
+	// var num1 = []int{1, 2, 3}       // Source slice
+	// var num2 = make([]int, len(num1)) // Destination slice with the same length as `num1`
+	// copy(num2, num1)                // Copying elements from `num1` to `num2`
+	// fmt.Println(num1)               // Output: [1 2 3]
+	// fmt.Println(num2)               // Output: [1 2 3] (num2 has the same elements as num1)
+}
